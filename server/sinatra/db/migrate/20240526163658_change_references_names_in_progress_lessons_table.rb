eeddf0b3e1883d69e05127b9b20a1aa129d313e7
eg-v <136094198+eg-v@users.noreply.github.com>
2024-05-26 14:17:46 -0300

    Add ChangeReferencesNamesInProgressLessonsTable migration

---
+++ server/sinatra/db/migrate/20240526163658_change_references_names_in_progress_lessons_table.rb
@@ -0,0 +1,7 @@
+class ChangeReferencesNamesInProgressLessonsTable < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :progress_lessons, :users_id, :user_id
+    rename_column :progress_lessons, :lessons_id, :lesson_id
+    rename_column :progress_lessons, :levels_id, :level_id
+  end
+end
