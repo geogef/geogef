98a392b4975292d8f772929c1e7dfa09815c37b9
eg-v <136094198+eg-v@users.noreply.github.com>
2024-05-26 14:19:14 -0300

    Update quiz so it actually shows an exam

--- server/sinatra/public/js/quiz.js
+++ server/sinatra/public/js/quiz.js
@@ -1,3 +1,9 @@
+let qaIds = [];
+let currentQaIndex = 0;
+let correctAnswers = 0;
+let examId;
+
+
 function fetchQuestionAndOptions(qaId) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', '/api/qa/' + qaId);
@@ -5,7 +11,7 @@ function fetchQuestionAndOptions(qaId) {
     xhr.onload = function() {
         if (xhr.status === 200) {
             var data = JSON.parse(xhr.responseText);
-            displayQuestionAndOptions(qaId, data.question, data.options , data.img);
+            displayQuestionAndOptions(qaId, data.question, data.options, data.img);
         } else {
             console.error('Request failed. Status:', xhr.status);
         }
@@ -26,7 +32,6 @@ function loadSVG(url) {
     return null;
 }
 
-
 function displayQuestionAndOptions(qaId, question, options, img) {
     document.getElementById('question').textContent = question;
 
@@ -43,14 +48,16 @@ function displayQuestionAndOptions(qaId, question, options, img) {
     options.forEach(function(option) {
         var button = document.createElement('button');
         button.textContent = option;
-        button.classList.add('answer-button', 'bg-gray-200', 'hover:bg-gray-300', 'text-gray-800', 'font-semibold', 'py-2', 'px-4', 'rounded', 'mr-2');
+        button.classList.add('answer-button', 'bg-[#e6edf8]', 'hover:bg-[#c7d9f0]', 'font-semibold', 'py-2', 'px-4', 'rounded', 'mr-2', 'transition', 'duration-300');
         button.onclick = function() {
             checkAnswer(qaId, option);
         };
         optionsContainer.appendChild(button);
     });
-}
 
+    document.getElementById('next-question').disabled = true;
+    document.getElementById('next-question').classList.add('bg-fun-blue-950');
+}
 
 function checkAnswer(qaId, userAnswer) {
     var xhr = new XMLHttpRequest();
@@ -65,24 +72,101 @@ function checkAnswer(qaId, userAnswer) {
             buttons.forEach(function(btn) {
                 btn.classList.remove('bg-green-500', 'hover:bg-green-500', 'bg-red-500', 'hover:bg-red-500');
                 if (btn.textContent === correctAnswer) {
-                    btn.classList.remove('bg-gray-200', 'hover:bg-gray-300');
+                    btn.classList.remove('bg-[#e6edf8]', 'hover:bg-[#c7d9f0]');
                     btn.classList.add('bg-green-500', 'hover:bg-green-400');
                 }
             });
 
             var selectedButton = [...buttons].find(btn => btn.textContent === userAnswer);
             if (selectedButton) {
-                selectedButton.classList.remove('hover:bg-gray-300');
+                selectedButton.classList.remove('bg-[#e6edf8]', 'hover:bg-[#c7d9f0]');
                 if (userAnswer === correctAnswer) {
+                    correctAnswers++;
                     selectedButton.classList.add('bg-green-500', 'hover:bg-green-400');
                 } else {
-                    selectedButton.classList.add('bg-red-500', 'hover:bg-red-400');
+                    selectedButton.classList.add('bg-red-500');
                 }
             }
 
+            document.getElementById('next-question').disabled = false;
+            document.getElementById('next-question').classList.remove('bg-fun-blue-950');
         } else {
             console.error('Request failed. Status:', xhr.status);
         }
     };
     xhr.send();
 }
+
+
+function checkLevelUp() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', `/api/exam/${examId}/${correctAnswers}`);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var data = JSON.parse(xhr.responseText);
+            document.getElementById('question').style.display = 'none';
+            document.getElementById('center-svg-container').style.display = 'none';
+            document.getElementById('options').style.display = 'none';
+            document.getElementById('next-question').style.display = 'none';
+            document.getElementById('completion-message').style.display = 'block';
+
+            if (data.message === 'Level up!') {
+                document.getElementById('completion-message').innerHTML = `
+                    <p class="text-center text-gray-600">Congratulations! You have completed the quiz.</p>
+                    <p class="text-center text-gray-600">You answered <span id="correct-answers">${correctAnswers}</span> questions correctly.</p>
+                    <p class="text-center text-gray-600">You have been promoted to the next level: ${data.new_level}.</p>
+                    <p class="text-center text-gray-600">Thank you for participating.</p>`;
+            } else {
+                document.getElementById('completion-message').innerHTML = `
+                    <p class="text-center text-gray-600">Congratulations! You have completed the quiz.</p>
+                    <p class="text-center text-gray-600">You answered <span id="correct-answers">${correctAnswers}</span> questions correctly.</p>
+                    <p class="text-center text-gray-600">${data.message}</p>
+                    <p class="text-center text-gray-600">Thank you for participating.</p>`;
+            }
+        } else {
+            console.error('Request failed. Status:', xhr.status);
+        }
+    };
+    xhr.send();
+}
+
+// Function to fetch QA IDs from the API endpoint
+function fetchQaIds(Id) {
+    examId = Id;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/exam/' + examId);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var data = JSON.parse(xhr.responseText);
+            qaIds = data.qas; // Store the retrieved QA IDs
+            fetchNextQuestion(examId); // Start fetching questions for the first QA ID
+        } else {
+            console.error('Request failed. Status:', xhr.status);
+        }
+    };
+    xhr.send();
+}
+
+// Function to fetch and display the next question
+function fetchNextQuestion() {
+    console.log(examId);
+    if (currentQaIndex < qaIds.length) {
+        var qaId = qaIds[currentQaIndex];
+        fetchQuestionAndOptions(qaId); // Fetch question for the current QA ID
+        currentQaIndex++; // Move to the next QA ID for the next iteration
+    } else {
+        console.log('All questions fetched.');
+        document.getElementById('question').style.display = 'none';
+        document.getElementById('center-svg-container').style.display = 'none';
+        document.getElementById('options').style.display = 'none';
+        document.getElementById('next-question').style.display = 'none';
+        document.getElementById('completion-message').style.display = 'block';
+        document.getElementById('correct-answers').textContent = correctAnswers;
+        checkLevelUp(examId);
+    }
+}
+
+document.getElementById('next-question').addEventListener('click', fetchNextQuestion);
+
