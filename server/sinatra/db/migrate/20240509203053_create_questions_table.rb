e4c6142018422a1a2d705b38b26fd5c9fe998902
florh1968 <florenciah1968@gmail.com>
2024-05-29 11:19:03 -0300

    Modify questions table

--- server/sinatra/db/migrate/20240509203053_create_questions_table.rb
+++ server/sinatra/db/migrate/20240509203053_create_questions_table.rb
@@ -2,7 +2,7 @@ class CreateQuestionsTable < ActiveRecord::Migration[7.1]
   def change
     create_table :questions do |t|
       t.string :question
-      t.references :topics, foreign_key: true
+      t.references :topic, foreign_key: true
       t.references :exams, foreign_key: true
       t.timestamps
     end
