334687e7e178907cae11977b5ddd95d56d6a0b45
florh1968 <florenciah1968@gmail.com>
2024-05-29 11:21:12 -0300

    Modify materials table

--- server/sinatra/db/migrate/20240509204254_create_materials_table.rb
+++ server/sinatra/db/migrate/20240509204254_create_materials_table.rb
@@ -2,7 +2,7 @@ class CreateMaterialsTable < ActiveRecord::Migration[7.1]
   def change
     create_table :materials do |t|
       t.string :content
-      t.references :learning, foreign_key: true
+      t.references :level, foreign_key: true
       t.timestamps
     end
   end
