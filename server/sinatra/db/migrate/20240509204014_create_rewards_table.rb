cccf7a6027972333f4f5ba9d1199944d6baccb2b
Gonzatrepin8 <Gonzatrepin0801@gmail.com>
2024-06-17 21:17:11 -0300

    delete rewards files

--- server/sinatra/db/migrate/20240509204014_create_rewards_table.rb
+++
@@ -1,9 +0,0 @@
-class CreateRewardsTable < ActiveRecord::Migration[7.1]
-  def change
-    create_table :rewards do |t|
-      t.string :name
-      t.integer :cost
-      t.timestamps
-    end
-  end
-end
